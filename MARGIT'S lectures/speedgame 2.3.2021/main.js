78b15e41963376017b534866e0c847c4e25c0c04
Iolanta Ubozhenko <iolanta.u@gmail.com>
2021-03-10 12:39:25 +0200

    speedgame: buttons visibility, not clickable circles before game, game ends after clicking wrong

--- MARGIT'S lectures/speedgame 2.3.2021/main.js
+++ MARGIT'S lectures/speedgame 2.3.2021/main.js
@@ -3,12 +3,17 @@ const numberOfCircles = circles.length;
 const scoreEl = document.querySelector(".score");
 const modal = document.querySelector(".modal");
 const result = document.querySelector(".result");
-const restartButton = document.querySelector(".restart__button");
-const stopButton = document.querySelector(".stop__button");
+const restartButton = document.querySelector(".restart_button");
+const stopButton = document.querySelector(".stop_button");
 let score = 0;
 let clearTimeoutId;
+document.querySelector(".stop_button").style.visibility = "hidden";
 
 const startGame = () => {
+  //// settingg visibility to start and end buttons
+  document.querySelector(".start_button").style.visibility = "hidden";
+  document.querySelector(".stop_button").style.visibility = "visible";
+  document.querySelector(".circles_container").style.cursor = "pointer";
   //pick a random circle
   let randomNumber = Math.floor(Math.random() * numberOfCircles);
   //initiate the circle
@@ -17,7 +22,24 @@ const startGame = () => {
   let previousNumber;
   let timer = 1000;
 
-  const myFunc = () => {
+  //if the user click on the active circle, update the score
+  circles.forEach((circle) => {
+    circle.addEventListener("click", () => {
+      if (circle.classList.contains("active")) {
+        score++;
+        scoreEl.textContent = `Your score is: ${score}`;
+      } else {
+        //user clicks on the wrong circle and game ends
+        stopGame("Sorry You Lose!!! Your Final Score is: ");
+        scoreEl.textContent = `Your score is: ${score}`;
+        if (score < 0) {
+          stopGame("Sorry You Lose!!! Your Final Score is: ");
+        }
+      }
+    });
+  });
+
+  const timerFunction = () => {
     //If the timer is less than 0, stop the game
     if (timer <= 0) {
       stopGame("Sorry you lose!! Your Final Score is: ");
@@ -39,34 +61,16 @@ const startGame = () => {
     //add active state to the random circle
     circles[randomNumber].classList.add("active");
     //call the function itself after timer of time
-    clearTimeoutId = setTimeout(myFunc, timer);
+    clearTimeoutId = setTimeout(timerFunction, timer);
   };
-  setTimeout(myFunc, timer);
+  setTimeout(timerFunction, timer);
 };
 
 //Event listener on start game button
-const startButton = document.querySelector(".start__button");
+const startButton = document.querySelector(".start_button");
 startButton.addEventListener("click", () => {
   console.log("start game");
   startGame();
-});
-
-//if the user click on the active circle, update the score
-circles.forEach((circle) => {
-  circle.addEventListener("click", () => {
-    if (circle.classList.contains("active")) {
-      score++;
-      scoreEl.textContent = `Your score is: ${score}`;
-    } else {
-      //user clicks on the wrong circle
-      //decrement the score
-      score--;
-      scoreEl.textContent = `Your score is: ${score}`;
-      if (score < 0) {
-        stopGame("Sorry You Lose!!! Your Final Score Is: ");
-      }
-    }
-  });
 });
 
 //function stop the game
@@ -75,7 +79,7 @@ const stopGame = (message) => {
   clearTimeout(clearTimeoutId);
   //popup the score
   modal.classList.add("show");
-  result.textContent = `${message}: ${score}`;
+  result.textContent = `${message} ${score}`;
 };
 
 //stop the game
