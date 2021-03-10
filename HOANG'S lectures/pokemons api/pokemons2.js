969730b8053aca5000367072aa83f60669649524
Iolanta Ubozhenko <iolanta.u@gmail.com>
2021-03-10 10:50:27 +0200

    pokedex final versions

---
+++ HOANG'S lectures/pokemons api/pokemons2.js
@@ -0,0 +1,32 @@
+(function () {
+  window.addEventListener("DOMContentLoaded", executeScript);
+
+  const POKEMON_URL = "http://pokeapi.co/api/v2/pokemon?limit=100&offset=200";
+  let pokemons = [];
+
+  async function executeScript() {
+    let json = await (await fetch(POKEMON_URL)).json();
+    pokemons = json.results;
+    pokemons.forEach(addPokemonEl);
+
+    document.querySelectorAll(".pokemon .name").forEach((el) => {
+      el.addEventListener("click", async (e) => {
+        let detailsObj = await (
+          await fetch(e.target.getAttribute("data-url"))
+        ).json();
+        console.log(detailsObj.sprites.front_default);
+        document
+          .getElementById("current-pokemon")
+          .setAttribute("src", detailsObj.sprites.front_default);
+      });
+    });
+  }
+
+  function addPokemonEl(pokemonObj) {
+    let newPokemonEl = document.createElement("div");
+    newPokemonEl.className = "pokemon";
+    newPokemonEl.innerHTML = `<div class="name" data-url="${pokemonObj.url}"> ${pokemonObj.name} </div>`;
+
+    document.getElementById("list").appendChild(newPokemonEl);
+  }
+})();
